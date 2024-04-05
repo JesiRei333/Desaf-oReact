0f8d713815c8efec6ee5d3f84f5d7461bb6b571e
Jesica Reyes <miucomosmos333@gmail.com>
2024-04-05 02:56:54 -0600

    se add boton de post

--- pages/nav.jsx
+++ pages/nav.jsx
@@ -80,6 +80,11 @@ export default function Nav() {
               Create account
             </button>
           </Link>
+          <Link href="/post">
+            <button className="hover:bg-[rgb(59_73_223)] p-[8px_16px] min-w-[95px] text-center text-[rgb(59_73_223)] hover:text-[rgb(255_255_255)] border-[rgb(59_73_223)] border-2 rounded-lg  font-semibold">
+              post
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
